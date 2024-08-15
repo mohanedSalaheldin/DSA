ead8c2119736ba1ba12681d1135b7767fe5a1536
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-08-15 23:03:19 +0300

    isPalindrome

--- src/linkedList/NewProblems.java
+++ src/linkedList/NewProblems.java
@@ -2,6 +2,7 @@ package linkedList;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class NewProblems {
     public ListNode middleNode(ListNode head) {
@@ -53,6 +54,60 @@ public class NewProblems {
 
     }
 
+    public ListNode findMiddle(ListNode head) {
+        if(head==null || head.next==null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+        while(cur!=null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode mid = findMiddle(head);
+        ListNode secondNode = mid.next;
+        secondNode = reverseList(secondNode);
+        ListNode cur1 = head;
+        ListNode cur2 = secondNode;
+        while(cur1!=null && cur2!=null){
+            if (cur1.val != cur2.val) {
+                return false;
+            }
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return true;
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null) return null;
+        ListNode currentOddNode = head;
+        ListNode newOddNode = head;
+        ListNode prevToNewOddNode = head;
+        while (currentOddNode!=null && currentOddNode.next!=null){
+            newOddNode = currentOddNode.next.next;
+            prevToNewOddNode = currentOddNode.next;
+            ListNode tempCur = currentOddNode;
+            tempCur.next = newOddNode;
+            prevToNewOddNode.next = newOddNode.next;
+            newOddNode.next = prevToNewOddNode;
+            currentOddNode = currentOddNode.next.next;
+        }
+        return  head;
+    }
     public static ListNode detectCycle(ListNode head) {
         Map<Integer, Integer> hp = new HashMap<>();
         ListNode cur = head;
